8f6f3482dd83bb4062ce6c71f8bcb6aff1ca7dbd
JanneSickert <janne.sickert@gmx.de>
2021-06-24 20:56:39 +0200

    added KeyHandler for enter

--- src/ui/Gui.java
+++ src/ui/Gui.java
@@ -6,6 +6,8 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -39,7 +41,7 @@ public class Gui implements UI {
 			add(panel);
 			setVisible(true);
 		}
-		
+
 		public void end() {
 			this.setVisible(false);
 		}
@@ -51,8 +53,8 @@ public class Gui implements UI {
 
 	@Override
 	public SettingType getSettings() {
-		JPanel panel = new JPanel();
 		final int NR_ELEMENTS = 4;
+		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1, 4));
 		JButton[] button = { new JButton(SettingType.COPY_ONLY.getName()), new JButton(SettingType.PASSWORD.getName()),
 				new JButton(SettingType.KEY_FILE.getName()), new JButton(SettingType.PASSWORD_AND_KEY_FILE.getName()) };
@@ -115,13 +117,15 @@ public class Gui implements UI {
 			JButton enter = new JButton("ENTER");
 			enter.setLocation(X * 2, 200);
 			enter.setSize(X, Y);
-			enter.addActionListener(new ActionListener() {
+			ActionListener lal = new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					path = jtf.getText();
 					messageNotSent = false;
 				}
-			});
+			};
+			enter.addActionListener(lal);
+			jtf.addKeyListener(new KeyHandler(lal));
 			this.add(label);
 			this.add(jtf);
 			this.add(enter);
@@ -132,6 +136,33 @@ public class Gui implements UI {
 		}
 	}
 	
+	class KeyHandler implements KeyListener{
+
+		private ActionListener al;
+		
+		KeyHandler(ActionListener al) {
+			this.al = al;
+		}
+		
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			switch (arg0.getKeyCode()) {
+			case KeyEvent.VK_ENTER:
+				al.actionPerformed(null);
+				break;
+			default:
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+		}
+	}
+
 	class GetPassword extends JPanel {
 
 		private static final long serialVersionUID = -3384453354229902144L;
@@ -150,13 +181,15 @@ public class Gui implements UI {
 			JButton enter = new JButton("ENTER");
 			enter.setLocation(X * 2, 200);
 			enter.setSize(X, Y);
-			enter.addActionListener(new ActionListener() {
+			ActionListener lal = new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					password = new String(jpf.getPassword());
 					messageNotSent = false;
 				}
-			});
+			};
+			enter.addActionListener(lal);
+			jpf.addKeyListener(new KeyHandler(lal));
 			this.add(label);
 			this.add(jpf);
 			this.add(enter);
@@ -166,7 +199,7 @@ public class Gui implements UI {
 			return password;
 		}
 	}
-	
+
 	private String askPath(String message) {
 		GetPath gp = new GetPath(message);
 		MyFrame frame = new MyFrame(gp);
@@ -253,12 +286,12 @@ public class Gui implements UI {
 	public String getRecoveryOutputPath() {
 		return (askPath("Target directory of the data to be restored"));
 	}
-	
-	static class MovePanel extends JPanel{
-		
+
+	static class MovePanel extends JPanel {
+
 		private static final long serialVersionUID = -5491547277707113467L;
 		static final int X = 700, Y = 30;
-		
+
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
@@ -271,13 +304,13 @@ public class Gui implements UI {
 			g2d.fillRect(4, 4 * Y + 1, x_value, Y - 1);
 		}
 	}
-	
+
 	public class MoveGui {
-		
+
 		private MovePanel movePanel = new MovePanel();
 		private JLabel[] moveLabel = new JLabel[4];
 		public MyFrame frame;
-		
+
 		public MoveGui() {
 			movePanel.setLayout(null);
 			for (int i = 0; i < moveLabel.length; i++) {
@@ -293,10 +326,10 @@ public class Gui implements UI {
 			}
 			frame = new MyFrame(movePanel);
 		}
-		
-		@Check_IO(input = {"C:/from/testFile.txt", "C:/to/0"})
+
+		@Check_IO(input = { "C:/from/testFile.txt", "C:/to/0" })
 		public synchronized void setText(String from, String to) {
-			String[] text = {"Move:", from, "to:", to};
+			String[] text = { "Move:", from, "to:", to };
 			for (int i = 0; i < moveLabel.length; i++) {
 				moveLabel[i].setText(text[i]);
 			}
